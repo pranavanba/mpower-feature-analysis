e03c80e71a71cb38da9dc2a3d9010e6f8271df02
arytontediarjo <atediarjo@gmail.com>
2021-07-06 13:12:01 -0700

    remove limit

--- R/utils/reticulate_utils.R
+++ R/utils/reticulate_utils.R
@@ -23,7 +23,7 @@ parse_phoneInfo <- function(data){
 
 get_table <- function(syn, synapse_tbl, file_columns, uid, keep_metadata){
   # get table entity
-  entity <- syn$tableQuery(glue::glue("SELECT * FROM {synapse_tbl} LIMIT 5"))
+  entity <- syn$tableQuery(glue::glue("SELECT * FROM {synapse_tbl}"))
   
   # shape table
   table <- entity$asDataFrame() %>%
