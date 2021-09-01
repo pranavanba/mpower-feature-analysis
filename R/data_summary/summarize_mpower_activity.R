d5e7981a6bc4cecf75d2c7dd4d5be65896c1309e
arytontediarjo <atediarjo@gmail.com>
2021-08-31 17:29:43 -0700

    added tapping features

--- R/data_summary/summarize_mpower_activity.R
+++ R/data_summary/summarize_mpower_activity.R
@@ -6,6 +6,17 @@ synapser::synLogin()
 
 TAPPING_FEATURES <- "syn25692238"
 
+####################################
+#### instantiate github #### 
+####################################
+GIT_TOKEN_PATH <- "~/git_token.txt"
+GIT_REPO <- "aryontediarjo/summarize_mpower_activity.R"
+SCRIPT_PATH <- file.path("R", "notebook", "summarize_mpower_activity.R")
+
+setGithubToken(readLines(GIT_TOKEN_PATH))
+GIT_URL <- githubr::getPermlink(GIT_REPO, repositoryPath = SCRIPT_PATH)
+
+
 summarize_users <- function(data){
     data %>%
         dplyr::group_by(healthCode) %>%
@@ -36,12 +47,14 @@ agg_record <- feature %>%
     summarize_records() %>%
     readr::write_tsv(agg_record_filename)
 file <- synapser::File(agg_record_filename, parent = "syn25999253")
-synapser::synStore(file, activity = Activity(used = TAPPING_FEATURES)) 
+synapser::synStore(file, activity = Activity(used = TAPPING_FEATURES,
+                                             executed = GIT_URL)) 
 agg_user <- feature %>% 
     summarize_users() %>%
     readr::write_tsv(agg_user_filename)
 file <- synapser::File(agg_user_filename, parent = "syn25999253")
-synapser::synStore(file, activity = Activity(used = TAPPING_FEATURES)) 
+synapser::synStore(file, activity = Activity(used = TAPPING_FEATURES,
+                                             executed = GIT_URL)) 
 
 
 
