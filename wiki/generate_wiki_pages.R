3bbcaf9c6f8a36a845e6b268fddeeec1a518847b
arytontediarjo <atediarjo@gmail.com>
2021-10-13 16:01:21 -0700

    added wiki pages

--- wiki/generate_wiki_pages.R
+++ wiki/generate_wiki_pages.R
@@ -13,8 +13,16 @@ clean_cache <- function(){
 
 # knit wiki page
 knit2synapse::createAndKnitToFolderEntity(
-    file = "wiki/generate_tapping_feat_extraction_wiki.rmd",
+    file = "wiki/generate_tapping_feat_extraction_wiki.Rmd",
     parentId = "syn26215070", 
+    wikiName = "Tapping",
+    folderName = "Tapping"
+)
+
+# knit wiki page
+knit2synapse::createAndKnitToFolderEntity(
+    file = "wiki/generate_tapping_feat_processed_wiki.Rmd",
+    parentId = "syn26215072", 
     wikiName = "Tapping",
     folderName = "Tapping"
 )
